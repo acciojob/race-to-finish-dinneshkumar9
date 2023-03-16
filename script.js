793d190fce432438042f68483ec2e812348e2401
Dinesh kumar <dinneshkumar9@gmail.com>
2023-03-16 22:04:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 window.promises = [];
 
+window.promises = Array.from({ length: 5 }, () => new Promise(resolve => {
+  const time = Math.floor(Math.random() * 5) + 1;
+  setTimeout(() => resolve(`Promise resolved after ${time} seconds`), time * 1000);
+}));
+
+Promise.any(window.promises)
+  .then(result => document.getElementById("output").innerHTML = result);
+
+// Do not change the code above this
+// add your promises to the array `promises`
+
 // Do not change the code above this
 // add your promises to the array `promises`
